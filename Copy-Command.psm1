3761977052f1fac41f1efcc83206d2dad2f35199
jdhitsolutions <jhicks@jdhitsolutions.com>
2017-05-01 15:08:46 -0400

    added external help and minor formatting

--- Copy-Command.psm1
+++ Copy-Command.psm1
@@ -4,66 +4,12 @@
 copy a PowerShell command including parameters and help to 
 turn it into a wrapper or a proxy function
 
-run this in the PowerShell ISE for best results
+run this in the PowerShell ISE for best results or Visual Studio Code.
 #>
 
 Function Copy-Command {
 
-<#
-.Synopsis
-Copy a PowerShell command
-.Description
-This command will copy a PowerShell command, including parameters and help to a new user-specified command. You can use this to create a "wrapper" function or to easily create a proxy function. The default behavior is to create a copy of the command complete with the original comment-based help block.
 
-For best results, run this in the PowerShell ISE so that the copied command will be opened in a new tab.
-.Parameter Command
-The name of a PowerShell command, preferably a cmdlet but that is not a requirment. You can specify an alias and it will be resolved.
-
-.Parameter NewName
-Specify a name for your copy of the command. If no new name is specified, the original name will be used.
-
-.Parameter IncludeDynamic
-The command will only copy explicitly defined parameters unless you specify to include any dynamic parameters as well. If you copy a command and it seems to be missing parameters, re-copy and include dynamic parameters.
-
-.Parameter AsProxy
-Create a traditional proxy function.
-
-.Parameter UseForwardHelp
-By default the copy process will create a comment-based help block with the original command's help which you can then edit to meet your requirements. Or you can opt to retain the forwarded help links to the original command.
-.Example
-PS C:\> Copy-Command Get-Process Get-MyProcess 
-
-Create a copy of Get-Process called Get-MyProcess.
-.Example
-PS C:\> Copy-Command Get-Eventlog -asproxy -useforwardhelp
-
-Create a proxy function for Get-Eventlog and use forwarded help links.
-.Example
-PS C:\> Copy-Command Get-ADComputer Get-MyADComputer -includedynamic
-
-Create a wrapper function for Get-ADComputer called Get-MyADComputer. Due to the way the Active Directory cmdlets are written, most parameters appear to be dynamic so you need to include dynamic parameters otherwise there will be no parameters in the final function.
-
-.Notes
-Last Updated: March 28, 2016
-Version     : 1.0.1
-
-Learn more about PowerShell:
-http://jdhitsolutions.com/blog/essential-powershell-resources/
-
-  ****************************************************************
-  * DO NOT USE IN A PRODUCTION ENVIRONMENT UNTIL YOU HAVE TESTED *
-  * THOROUGHLY IN A LAB ENVIRONMENT. USE AT YOUR OWN RISK.  IF   *
-  * YOU DO NOT UNDERSTAND WHAT THIS SCRIPT DOES OR HOW IT WORKS, *
-  * DO NOT USE IT OUTSIDE OF A SECURE, TEST SETTING.             *
-  ****************************************************************
-
-.Link
-Get-Command
-.Inputs
-None
-.Outputs
-[string[]]
-#>
 
 [cmdletbinding()]
 Param(
